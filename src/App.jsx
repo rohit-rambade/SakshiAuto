12268ae40c892f3e9013e139ad7f4481aa54ba90
rohit-rambade <rohitrambade18@gmail.com>
2024-01-22 14:25:20 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/common/Footer";
+import Header from "./components/common/Header";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
